77077f3df21a4a7429fb32077d99a9b4b478cb7e
juanamador10 <juan.amador.is@unipolidgo.edu.mx>
2022-10-17 02:52:24 -0500

    Beginning
    Home Screen: App Bar Icons, Rows & Columns.

--- app_products_flutter/lib/screen/home_screen.dart
+++ app_products_flutter/lib/screen/home_screen.dart
@@ -9,15 +9,55 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
     
     return Scaffold(
        appBar: AppBar(
-        title: const Text('SQLite Example with Cats'), 
+        leading: IconButton(onPressed: () {}, icon: const Icon(Icons.arrow_back)),
+        //title: const Text('SQLite Example with Cats'), 
         elevation: 0,
-        backgroundColor: Colors.pink,),
+        backgroundColor: Colors.pink,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.shopping_bag_outlined)),
+        ],),
       body: Container(
-        child: Text("ssss"),
+        /*padding: const EdgeInsets.all(10.0),*/
+        margin: const EdgeInsets.all(10.0),
+        color: Colors.amber[600],
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          //shrinkWrap: true,
+          children: [
+            // - - - - - - - - - - Columns - - ->
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    const Text("data"),
+                    const Text("data"),
+                  ],
+                )
+              ],
+            ),
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    const Text("aaa"),
+                    const Text("daaaata"),
+                  ],
+                )
+              ],
+            ),
+          ],
+        )
       ),
     );
   }
