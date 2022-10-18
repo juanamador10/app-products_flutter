1d7c383b7cc44f78591763938aef10d968c15fc6
juanamador10 <juan.amador.is@unipolidgo.edu.mx>
2022-10-18 02:51:36 -0500

    Product Card

--- app_products_flutter/lib/screen/home_screen.dart
+++ app_products_flutter/lib/screen/home_screen.dart
@@ -1,32 +1,53 @@
+import 'package:app_products_flutter/models/product_card.dart';
+import 'package:app_products_flutter/models/product_show_card.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
+  //final String changing_text;
+
   const HomeScreen({Key? key}) : super(key: key);
+  
   
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 
 }
 
+String imageLink= "Hola";
+String name = "Name";
+String position = "Positon";
+
 class _HomeScreenState extends State<HomeScreen> {
+
+  prodcutChange(String nameChange, String positionChange) {
+       setState(() {name = nameChange; position = positionChange;});
+    }
+
   @override
   Widget build(BuildContext context) {
     
+
+    const textTitleStyle = TextStyle(fontSize: 18, color: Colors.brown);
+    const textTitleBoldStyle = TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: Colors.brown);
+    
     return Scaffold(
-       appBar: AppBar(
-        leading: IconButton(onPressed: () {}, icon: const Icon(Icons.arrow_back)),
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+        leading: IconButton(onPressed: () {}, icon: const Icon(Icons.arrow_back, color: Colors.brown,)),
         //title: const Text('SQLite Example with Cats'), 
         elevation: 0,
-        backgroundColor: Colors.pink,
+        backgroundColor: Colors.white,
         actions: [
           IconButton(
             onPressed: () {},
-            icon: const Icon(Icons.shopping_bag_outlined)),
+            icon: const Icon(Icons.shopping_bag_outlined, color: Colors.brown,)),
         ],),
+
+
       body: Container(
         /*padding: const EdgeInsets.all(10.0),*/
-        margin: const EdgeInsets.all(10.0),
-        color: Colors.amber[600],
+        margin: const EdgeInsets.all(20.0),
+        color: Colors.white,
         child: ListView(
           scrollDirection: Axis.vertical,
           //shrinkWrap: true,
@@ -38,23 +59,71 @@ class _HomeScreenState extends State<HomeScreen> {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   // ignore: prefer_const_literals_to_create_immutables
                   children: [
-                    const Text("data"),
-                    const Text("data"),
+                    const Text("Hi, Juan", style: textTitleStyle,),
                   ],
                 )
               ],
             ),
+
+            const SizedBox(height: 18,),
+
             Column(
               children: [
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   // ignore: prefer_const_literals_to_create_immutables
                   children: [
-                    const Text("aaa"),
-                    const Text("daaaata"),
+                    const Text("What's today taste?", style: textTitleBoldStyle,),
                   ],
                 )
               ],
+            ),
+            
+            const SizedBox(height: 18,),
+
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    Row(
+                      children: [
+                        IconButton(onPressed: () {}, icon: const Icon(Icons.fastfood_outlined), color: Colors.brown, iconSize: 35),
+                        const SizedBox(width: 15,),
+
+                        IconButton(onPressed: () {}, icon: const Icon(Icons.cake_outlined), color: Colors.brown, iconSize: 35),
+                      ],
+                    ),
+                    IconButton(onPressed: () {}, icon: const Icon(Icons.search_outlined), color: Colors.brown, iconSize: 35,),
+
+                  ],
+                )
+              ],
+            ),
+
+            const SizedBox(height: 18,),
+
+            ProductShowCard(name: name, position: position),
+
+            const SizedBox(height: 35,),
+
+            Column(
+              children: [
+                SingleChildScrollView(
+                  child: Row(
+              //scrollDirection: Axis.horizontal,
+              // ignore: prefer_const_literals_to_create_immutables - - - - - - - 
+              // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      TextButton(
+                        style: TextButton.styleFrom(foregroundColor: Colors.yellow),
+                        onPressed: () => {prodcutChange("Change name", "Change Position"), },
+                        child: const Text("Clickkk")),
+                    ],
+                  ),
+                ),
+              ]
             ),
           ],
         )
